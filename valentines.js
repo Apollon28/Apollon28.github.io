cdbb6184a89c83351eb2ebe9c86062c61944e4a9
Apollon28 <160219350+Apollon28@users.noreply.github.com>
2024-02-16 18:07:13 +0800

    Update valentines.js

--- valentines.js
+++ valentines.js
@@ -6,9 +6,6 @@ const maxHeight = window.innerHeight- buttonHeight;
 
 window.addEventListener( 'DOMContentLoaded', () => {
     const button =document.getElementById('button');
-
-    button.addEventListener('click', () => alert("Yow")) ;
-
     button.addEventListener('mouseover', () => {
         button.style.left = Math.floor(Math.random() * (maxWidth + 1)) + 'px';
         button.style.top = Math.floor(Math.random() * (maxHeight+ 1)) + 'px';
